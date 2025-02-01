53df72331aa6d98c838ebd430f99039dfe7b12a5
Assadbek Ravshanbekov <asadbekravshanbek70@gmail.com>
2025-02-01 21:33:43 +0300

    Add files via upload

---
+++ lab2/boolean.py
@@ -0,0 +1,14 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+
+x = "Hello"
+y = 15
+print(bool(x))
+print(bool(y))
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple", "cherry", "banana"]))
+print(bool([]))
+print(bool({}))
